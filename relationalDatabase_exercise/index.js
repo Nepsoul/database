47f236d49e4f4d316820538905ba0e0e6e7f8ea9
nepsoul <nirumagar901@gmail.com>
2023-09-26 05:00:30 +0545

    refactor:  feat. try/catch method

--- relationalDatabase_exercise/index.js
+++ relationalDatabase_exercise/index.js
@@ -52,19 +52,47 @@ app.get("/api/blogs", async (req, res) => {
   res.json(blogs);
 });
 
-app.post("/api/blogs", async (req, res) => {
+app.get("/api/blogs/:id", async (req, res) => {
+  const blog = await Blog.findByPk(req.params.id);
+  if (blog) {
+    console.log(blog.toJSON());
+    res.json(blog);
+  } else {
+    res
+      .status(404)
+      .json({ message: `blog not found for this id ${req.params.id}` })
+      .end();
+  }
+});
+
+app.post("/api/blogs/", async (req, res) => {
   // console.log(req.body);
 
-  const blog = await Blog.create(req.body);
-  console.log(blog);
-  res.json(blog);
+  try {
+    const blog = await Blog.create(req.body);
+    console.log(blog);
+    res.json(blog);
+  } catch (error) {
+    return res.status(400).json({ error }).end();
+  }
 });
 
 app.delete("/api/blogs/:id", async (req, res) => {
-  // const id = req.params.id;
-  const blog = await Blog.destroy({ where: { id: req.params.id } }); //sql query
-  res.json(blog);
+  // const blog = await Blog.destroy({ where: { id: req.params.id } }); //sql query
+  // res.json(blog);
+
+  const id = req.params.id;
+  try {
+    const result = Blog.findByPk(id); //sequalize method
+    if (result) {
+      await result.destroy();
+    }
+    res.status(204).json({ message: "blog deleted successfully" }).end();
+  } catch (error) {
+    res.status(500).send("error occured while deleting the blog").end();
+  }
 });
+
 const PORT = process.env.PORT || 3003;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
