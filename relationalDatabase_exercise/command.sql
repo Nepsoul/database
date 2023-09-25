8bdf176e4f36a4dc5bddb26f9ff3c01684d6c7f2
nepsoul <nirumagar901@gmail.com>
2023-09-25 15:50:51 +0545

    chore: initialize of blog database

---
+++ relationalDatabase_exercise/command.sql
@@ -0,0 +1,15 @@
+-- create blog table
+CREATE TABLE blogs (
+    id SERIAL PRIMARY KEY,
+    title text NOT NULL,
+    url text NOT NULL,
+    author text NOT NULL,
+    likes integer NULL
+   );
+
+   -- select the blog table
+   select * from blogs
+
+      -- data insert into blog table
+insert into blogs (title, url, author, likes) values ('Relational databases rule the world', 'https://himalayan.com', 'ram', 3);
+insert into blogs (title, url, author, likes) values ('MongoDB is webscale', 'http://himalayan.com', 'sita', 8);
