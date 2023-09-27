f83dd1239daa90a8a49ab91200c3792185da5444
nepsoul <nirumagar901@gmail.com>
2023-09-27 10:37:33 +0545

    part 13-b: user management: feat. jwt token, user's model,controller, Model.sync()

--- relationalDatabase_workshop/index.js
+++ relationalDatabase_workshop/index.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const app = express();
 const noteRouter = require("./controllers/notes");
+const loginRouter = require("./controllers/login");
+const userRouter = require("./controllers/users");
 const { PORT } = require("./utils/config");
 const { connectToDatabase } = require("./utils/db");
 
 app.use(express.json()); //json parser
 
 app.use("/api/notes", noteRouter);
+app.use("/api/login", loginRouter);
+app.use("/api/users", userRouter);
 
 //wrapping connection into async function for secure connection to database
 const start = async () => {
--- relationalDatabase_workshop/models/index.js
+++ relationalDatabase_workshop/models/index.js
@@ -1,6 +1,8 @@
 const Note = require("./note");
+const User = require("./user");
 
 //generate schema automatically from defined model, if table does not exist
 Note.sync();
+User.sync();
 
-module.exports = { Note };
+module.exports = { Note, User };
