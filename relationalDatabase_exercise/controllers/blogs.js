b9c05366ddfba3660fba8d649a5cb4693fb4b333
nepsoul <nirumagar901@gmail.com>
2023-10-02 16:50:23 +0545

    EX: 13.5: refactor: moving model, api, connection to database to blogs.js file

---
+++ relationalDatabase_exercise/controllers/blogs.js
@@ -0,0 +1,98 @@
+require("dotenv").config();
+const app = require("express").Router();
+
+const { Sequelize, Model, DataTypes } = require("sequelize");
+
+//connecting to database
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+});
+
+class Blog extends Model {}
+Blog.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    url: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    author: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    likes: {
+      type: DataTypes.INTEGER,
+      default: 0,
+    },
+  },
+  {
+    sequelize,
+    underscored: true,
+    timestamps: false,
+    modelName: "blog",
+  }
+);
+
+Blog.sync();
+
+app.get("/", async (req, res) => {
+  const blogs = await Blog.findAll();
+  res.json(blogs);
+});
+
+app.get("/:id", async (req, res) => {
+  const blog = await Blog.findByPk(req.params.id);
+  if (blog) {
+    console.log(blog.toJSON());
+    res.json(blog);
+  } else {
+    res
+      .status(404)
+      .json({ message: `blog not found for this id ${req.params.id}` })
+      .end();
+  }
+});
+
+app.post("/", async (req, res) => {
+  // console.log(req.body);
+
+  try {
+    const blog = await Blog.create(req.body);
+    console.log(blog);
+    res.json(blog);
+  } catch (error) {
+    return res.status(400).json({ error }).end();
+  }
+});
+
+app.delete("/:id", async (req, res) => {
+  //sequalize method using sql query, converting into sql query
+  // const blog = await Blog.destroy({ where: { id: req.params.id } });
+  // res.json(blog);
+
+  const id = req.params.id;
+  try {
+    const result = Blog.findByPk(id); //sequalize method
+    if (result) {
+      await result.destroy();
+    }
+    res.status(204).json({ message: "blog deleted successfully" }).end();
+  } catch (error) {
+    res.status(500).send("error occured while deleting the blog").end();
+  }
+});
+
+module.exports = app;
--- relationalDatabase_exercise/index.js
+++ relationalDatabase_exercise/index.js
@@ -1,97 +1,10 @@
-require("dotenv").config();
-const { Sequelize, Model, DataTypes } = require("sequelize");
-
-const express = require("express");
+const express = require("express"); //parse json request
 const app = express();
+const blogRouter = require("./controllers/blogs");
 
-app.use(express.json());
+app.use(express.json()); //json parser, to parse req.body's data
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false,
-    },
-  },
-});
-
-class Blog extends Model {}
-Blog.init(
-  {
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    title: {
-      type: DataTypes.TEXT,
-      allowNull: false,
-    },
-    url: {
-      type: DataTypes.TEXT,
-      allowNull: false,
-    },
-    author: {
-      type: DataTypes.TEXT,
-      allowNull: false,
-    },
-    likes: {
-      type: DataTypes.INTEGER,
-      default: 0,
-    },
-  },
-  {
-    sequelize,
-    underscored: true,
-    timestamps: false,
-    modelName: "blog",
-  }
-);
-app.get("/api/blogs", async (req, res) => {
-  const blogs = await Blog.findAll();
-  res.json(blogs);
-});
-
-app.get("/api/blogs/:id", async (req, res) => {
-  const blog = await Blog.findByPk(req.params.id);
-  if (blog) {
-    console.log(blog.toJSON());
-    res.json(blog);
-  } else {
-    res
-      .status(404)
-      .json({ message: `blog not found for this id ${req.params.id}` })
-      .end();
-  }
-});
-
-app.post("/api/blogs/", async (req, res) => {
-  // console.log(req.body);
-
-  try {
-    const blog = await Blog.create(req.body);
-    console.log(blog);
-    res.json(blog);
-  } catch (error) {
-    return res.status(400).json({ error }).end();
-  }
-});
-
-app.delete("/api/blogs/:id", async (req, res) => {
-  // const blog = await Blog.destroy({ where: { id: req.params.id } }); //sql query
-  // res.json(blog);
-
-  const id = req.params.id;
-  try {
-    const result = Blog.findByPk(id); //sequalize method
-    if (result) {
-      await result.destroy();
-    }
-    res.status(204).json({ message: "blog deleted successfully" }).end();
-  } catch (error) {
-    res.status(500).send("error occured while deleting the blog").end();
-  }
-});
+app.use("/api/blogs", blogRouter);
 
 const PORT = process.env.PORT || 3003;
 app.listen(PORT, () => {
