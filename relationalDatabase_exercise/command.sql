88d4a69ef2beb47cf3f4511cd21f4d2a651b71ed
nepsoul <nirumagar901@gmail.com>
2023-11-01 14:24:49 +0545

    info: add more sql query

--- relationalDatabase_exercise/command.sql
+++ relationalDatabase_exercise/command.sql
@@ -7,9 +7,16 @@ CREATE TABLE blogs (
     likes int default 0
    );
 
-   -- select the blog table
+-- select the blog table
    select * from blogs
 
-      -- data insert into blog table
+-- data insert into blog table
 insert into blogs (title, url, author, likes) values ('Relational databases rule the world', 'https://himalayan.com', 'ram', 3);
 insert into blogs (title, url, author, likes) values ('MongoDB is webscale', 'http://himalayan.com', 'sita', 8);
+
+-- to delete blog
+drop table blogs
+
+--to alter the data type
+ALTER TABLE blogs
+ALTER COLUMN likes TYPE INTEGER USING likes::integer;
