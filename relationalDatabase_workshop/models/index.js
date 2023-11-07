46859aafbb04a410670fa82616f6d51c9f9b7be9
nepsoul <nirumagar901@gmail.com>
2023-11-07 14:42:40 +0545

    part 13-c: add migration to keep track on changes in database in controlled manner stored in version control, install umzug library

--- relationalDatabase_workshop/models/index.js
+++ relationalDatabase_workshop/models/index.js
@@ -6,7 +6,7 @@ User.hasMany(Note);
 Note.belongsTo(User);
 
 //generate schema automatically from defined model, if table does not exist
-Note.sync({ alter: true }); //if defined model altered, then it auto generate table according to altered model
-User.sync({ alter: true });
+// Note.sync({ alter: true }); //if defined model altered, then it auto generate table according to altered model
+// User.sync({ alter: true });
 
 module.exports = { Note, User };
--- relationalDatabase_workshop/utils/db.js
+++ relationalDatabase_workshop/utils/db.js
@@ -1,5 +1,6 @@
 const { Sequelize } = require("sequelize");
 const { DATABASE_URL } = require("./config");
+const { Umzug, SequelizeStorage } = require("umzug");
 
 const sequelize = new Sequelize(DATABASE_URL, {
   dialectOptions: {
@@ -10,10 +11,26 @@ const sequelize = new Sequelize(DATABASE_URL, {
   },
 });
 
+//sequelize keep track on changes of migration, if not change in migration, does nothing
+const runMigrations = async () => {
+  const migrator = new Umzug({
+    migrations: { glob: "migrations/*.js" },
+    storage: new SequelizeStorage({ sequelize, tableName: "migrations" }),
+    context: sequelize.getQueryInterface(),
+    logger: console,
+  });
+
+  const migrations = await migrator.up();
+  console.log("migration up to date", {
+    files: migrations.map((mig) => mig.name),
+  });
+};
+
 // to secure database connection before actual startup
 const connectToDatabase = async () => {
   try {
     await sequelize.authenticate();
+    await runMigrations();
     console.log("connected to database");
   } catch (err) {
     console.log("failed to connect to database");
