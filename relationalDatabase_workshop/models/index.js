bf8f5233dcc57ac8bc3a3321ee0dea1d283558cc
nepsoul <nirumagar901@gmail.com>
2023-11-08 15:48:49 +0545

    part 13-c: add many-to-many relation between note and user through usernote table, add author in marked notes table

--- relationalDatabase_workshop/models/index.js
+++ relationalDatabase_workshop/models/index.js
@@ -3,6 +3,7 @@ const User = require("./user");
 
 const Team = require("./team");
 const Membership = require("./membership");
+const UserNotes = require("./UserNotes");
 
 // defining relation of User and Note
 User.hasMany(Note);
@@ -11,6 +12,9 @@ Note.belongsTo(User);
 //many-to-many relationships
 User.belongsToMany(Team, { through: Membership });
 Team.belongsToMany(User, { through: Membership });
+
+User.belongsToMany(Note, { through: UserNotes, as: "marked_notes" });
+Note.belongsToMany(User, { through: UserNotes, as: "users_marked" });
 
 //generate schema automatically from defined model, if table does not exist
 // Note.sync({ alter: true }); //if defined model altered, then it auto generate table according to altered model
